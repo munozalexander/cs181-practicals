8011f98824edf4b25dfa5504804ea150cf60d675
davey <davidralphhughes@college.harvard.edu>
2018-05-02 23:40:15 -0400

    Good tabular Q learning

--- P4/stub.py
+++ P4/stub.py
@@ -2,6 +2,8 @@
 import numpy as np
 import numpy.random as npr
 import pygame as pg
+import pickle
+import time
 
 from SwingyMonkey import SwingyMonkey
 
@@ -19,11 +21,11 @@ class Learner(object):
         self.screen_width = 600
         self.screen_height = 400
         self.max_vel = 60
-        self.n_bins = 4
-        self.gamma = 0.5
-        self.eta = .1
+        self.n_bins = 15
+        self.gamma = .5
+        self.eta = .9
         self.step = 0
-        self.epsilon_decay = 0.9
+        self.epsilon_decay = 0.999
         self.Q = np.zeros((self.n_bins, self.n_bins, self.n_bins, self.n_bins, 2))
 
     def epsilon(self):
@@ -50,7 +52,7 @@ class Learner(object):
 
         if self.first_tick:
             if self.last_state is not None:
-                self.gravity = state['monkey']['vel'] - self.last_state['monkey']['vel']
+                self.gravity = self.last_state['monkey']['vel'] - state['monkey']['vel']
                 self.first_tick = False
             self.last_state = new_state
             self.last_action = 0
@@ -59,12 +61,12 @@ class Learner(object):
         monkey_x = self.discretize(new_state['tree']['dist'], self.screen_width)
         monkey_y = self.discretize(new_state['monkey']['bot'], self.screen_height)
         monkey_v = self.discretize(new_state['monkey']['vel'], self.max_vel)
-        tree_y = self.discretize(new_state['tree']['bot'], self.screen_height)
+        tree_y = self.discretize(new_state['tree']['top'], self.screen_height)
 
         monkey_x_old = self.discretize(self.last_state['tree']['dist'], self.screen_width)
         monkey_y_old = self.discretize(self.last_state['monkey']['bot'], self.screen_height)
         monkey_v_old = self.discretize(self.last_state['monkey']['vel'], self.max_vel)
-        tree_y_old = self.discretize(self.last_state['tree']['bot'], self.screen_height)
+        tree_y_old = self.discretize(self.last_state['tree']['top'], self.screen_height)
 
         prev_q = self.Q[monkey_x_old, monkey_y_old, monkey_v_old, tree_y_old, self.last_action]
         grad_q = prev_q - (self.last_reward + self.gamma * np.max(self.Q[monkey_x, monkey_y, monkey_v, tree_y, :]))
@@ -89,6 +91,8 @@ def run_games(learner, hist, iters = 100, t_len = 100):
     '''
     Driver function to simulate learning by having the agent play a sequence of games.
     '''
+
+    timestamp = int(time.time())
     for ii in range(iters):
         # Make a new monkey object.
         swing = SwingyMonkey(sound=False,                  # Don't play sounds.
@@ -103,6 +107,18 @@ def run_games(learner, hist, iters = 100, t_len = 100):
 
         # Save score history.
         hist.append(swing.score)
+        print('epoch: ' + str(ii) + ', score: ' + str(swing.score) + ', gravity: ' + str(swing.gravity) + ', running_avg: ' + str(np.average(hist[-10:])))
+
+        results = {
+            'n_bins': learner.n_bins,
+            'gamma': learner.gamma,
+            'eta': learner.eta,
+            'epsilon_decay': learner.epsilon_decay,
+            'hist': hist
+        }
+
+        with open ('results/results_' + str(timestamp) + '.p', 'wb') as f:
+            pickle.dump(results, f)
 
         # Reset the state of the learner.
         learner.reset()
@@ -113,6 +129,6 @@ def run_games(learner, hist, iters = 100, t_len = 100):
 if __name__ == '__main__':
     agent = Learner()
     hist = []
-    run_games(agent, hist, 100, 2)
-    print hist
-    np.save('hist',np.array(hist))
+    run_games(agent, hist, 1000, 1)
+    print(hist)
+    np.save('hist', np.array(hist))
